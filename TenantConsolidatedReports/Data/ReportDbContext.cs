15afd5754447d2a0585048ade33612f4d2cc8345
Farhan Sodiq <sodiqfarhan360@gmail.com>
2024-10-21 20:21:20 +0100

    Add project files.

---
+++ TenantConsolidatedReports/Data/ReportDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TenantConsolidatedReports.Models.Entities;
+
+namespace TenantConsolidatedReports.Data
+{
+    public class ReportDbContext : DbContext
+    {
+        public ReportDbContext(DbContextOptions<ReportDbContext> dbContextOptions) : base(dbContextOptions)
+        {            
+        }
+
+        public DbSet<BusinessUnitReport> Reports { get; set; }
+    }
+}
---
+++ TenantConsolidatedReports/Models/Entities/BusinessUnitReport.cs
@@ -0,0 +1,18 @@
+﻿namespace TenantConsolidatedReports.Models.Entities
+{
+    public class BusinessUnitReport
+    {
+        public int Id { get; set; }
+        public int TenantId { get; set; }
+        public string TenantName { get; set; }
+        public string DisplayName { get; set; }
+        public int ParentId { get; set; }
+        public string ParentName { get; set; }
+        public int UnitHeadId { get; set; }
+        public string UnitHeadFullName { get; set; }
+        public int UnitId { get; set; }
+        public Boolean IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
